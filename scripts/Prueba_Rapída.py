beefc1fbc5b39c956aed86245fa2f7b0d7f28de9
Felipe Farías A. <felipe.farias@hakalab.com>
2026-04-22 10:27:55 -0400

    chore: update HakaBuddy tests

--- scripts/Prueba_Rapída.py
+++ scripts/Prueba_Rapída.py
@@ -7,21 +7,10 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    
-    # Navegación a la página principal
-    page.goto("https://www.correos.cl/")
-
-    # Navegar al centro de ayuda
-    page.get_by_role("link", name="Ayuda").click()
-    
-    # Realizar búsqueda de información
-    page.get_by_label("Buscar en preguntas frecuentes").fill("envio")
-    page.get_by_role("button", name="Buscar").click()
-    
-    # Seleccionar resultado y verificar contenido esperado
-    page.get_by_role("link", name=re.compile("¿Dónde puedo retirar mis enví")).click()
-    expect(page.get_by_text("Contáctanos")).to_be_visible()
-
+    page.goto(\"https://playwright.dev/\")
+    expect(page).to_have_title(re.compile(\"Playwright\"))
+    page.get_by_role(\"link\", name=\"Get started\").click()
+    expect(page).to_have_url(re.compile(\".*intro.*\"))
     context.close()
     browser.close()
 
